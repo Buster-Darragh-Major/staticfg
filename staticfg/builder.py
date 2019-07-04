0d9d2e37284689565c695c5a12da153c6c146d29
buster-darragh-major <busterjdarraghmajor@gmail.com>
2019-07-04 18:54:04 +1200

    Fix array acces chaining issue

--- staticfg/builder.py
+++ staticfg/builder.py
@@ -257,7 +257,7 @@ class CFGBuilder(ast.NodeVisitor):
             elif type(node) == ast.Str:
                 return node.s
             elif type(node) == ast.Subscript:
-                return node.value.id
+                return node.value.id if node.value.id else ''
             else:
                 return ''
 
